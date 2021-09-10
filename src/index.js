ff66c0dbacdc2049795f0518b827a95352e9418c
tmampa <tmampa94@gmail.com>
2021-09-10 12:01:14 +0200

    Add links

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
-import home from '../home';
-import about from '../about';
-import clear from '../clear';
-import menu from '../menu';
-import contact from '../contact';
+import home from './home';
+import about from './about';
+import clear from './clear';
+import menu from './menu';
+import contact from './contact';
 import './reset.css';
 import './style.css';
 
 home();
+
+// Home tab
+let homeBtn = document.getElementById('home');
+homeBtn.onclick = () => {
+  clear();
+  home();
+};
+
+// About tab
+let aboutBtn = document.getElementById('about');
+aboutBtn.onclick = () => {
+  clear();
+  about();
+};
+
+// Menu tab
+let menuBtn = document.getElementById('menu');
+menuBtn.onclick = () => {
+  clear();
+  menu();
+}
+
+// Contact tab
+let contactBtn = document.getElementById('contact');
+contactBtn.onclick = () => {
+  clear();
+  contact();
+}
